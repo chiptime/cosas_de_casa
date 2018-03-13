55ddbc8aa6810b451dc16a98829e007b559b08b9
ChipTime <amor95_8@hotmail.com>
2018-03-13 22:31:09 +0100

    Add files via upload

--- c-programs/examen/primo.cpp
+++ c-programs/examen/primo.cpp
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 10
+
 int main(){
 
+    int matriz[N];
+
+    int numero;
+    int divisores;
+
+    for(int i=0;i<N;i++){
+        matriz[N] = rand() % 20;
+        if(matriz[i]%i==0)
+            divisores++;
+    }
+    if(divisores==2)
+        printf(" %lf es primo", &numero);
+    else
+        printf(" %lf NO es primo", &numero);
 
 return EXIT_SUCCESS;
 
