7bdeb5fc084548cba07f5ba1a0fd279d288ad4a9
chiptime <amor95_8@hotmail.com>
2018-03-14 13:10:07 +0100

    paso por valor no es paso por referencia

--- c-programs/examen/funcion.cpp
+++ c-programs/examen/funcion.cpp
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void rellenar(int *n1, int *n2){
+
+        printf("Numero 1: ");
+        scanf(" %i", n1);
+        printf("Numero 2: ");
+        scanf(" %i", n2);
+    }
+
+
 int main(){
+
+    int v1,v2;
+
+   rellenar(&v1,&v2);
+        printf("Numeros: \n\t%i\n\t%i\n", v1,v2);
 
 
 return EXIT_SUCCESS;
 
 }
+
